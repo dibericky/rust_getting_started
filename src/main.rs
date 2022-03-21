96eb8cd05822c5fd2aff95a3d66b8c179feedf0c
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2022-03-21 23:46:03 +0100

    struct

--- src/main.rs
+++ src/main.rs
@@ -66,9 +66,115 @@ fn first_world_with_slice (s: &str) -> &str {
     &s[..]
 }
 
-fn main() {
+fn _main_slices() {
     let my_string = "okok fo";
     let word = first_world_with_slice(&my_string);
     println!("Ok {}", word);
 
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+struct Point(i32, i32, i32);
+struct Color(i32, i32, i32);
+
+fn build_user(email: String, username: String) -> User{
+    User{
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn _main_struct() {
+    let mut user1 = User {
+        email: String::from("someone@email.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("another@email.com");
+    let user2 = build_user(String::from("thenew@email.com"), String::from("the-username"));
+
+    let _user3 = User{
+        email: String::from("anew@email.com"),
+        ..user2
+    };
+    //println!("Nope, it's moved {}", user2.username)
+
+    let _origin = Point(0, 0, 0);
+    let _black = Color(0, 0, 0);
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main_rectangle() {
+    let scale = 2;
+    let rect1 = Rectangle{
+        width: dbg!(30 * scale),
+        height: 50
+    };
+    println!(
+        "The area of the retangle is {} square pixels.",
+        area(&rect1)
+    );
+    println!(
+        "The area of the retangle is {} square pixels...",
+        rect1.area()
+    );
+    println!("rect1 is {:?}", rect1);
+    dbg!(&rect1);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(10);
+    println!("The square {}", square.area())
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn main() {
+    main_rectangle()
 }
