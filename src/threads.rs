d1ef6a3fb4b56560692c78c2678cff84ac5b6df7
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2022-03-27 17:53:12 +0200

    mutex

--- src/threads.rs
+++ src/threads.rs
@@ -1,35 +1,70 @@
 use std::{thread, time};
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+
+fn _channels () {
+    let (tx, rx) = mpsc::channel();
+    let tx2 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(time::Duration::from_secs(1));
+ 
+        }
+    });
+    thread::spawn(move || {
+     let vals = vec![
+         String::from("more"),
+         String::from("messages"),
+         String::from("in"),
+         String::from("threadss"),
+     ];
+     for val in vals {
+         tx2.send(val).unwrap();
+         thread::sleep(time::Duration::from_secs(1));
+     }
+ });
+    for received in rx {
+        println!("Got {}", received);
+    }
+}
+
+fn mutex_basic () {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {:?}", m);
+}
+
+fn shared_states () {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move ||{
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Counter {:?}", counter);
+}
 
 pub fn try_threads () {
-   let (tx, rx) = mpsc::channel();
-   let tx2 = tx.clone();
-   thread::spawn(move || {
-       let vals = vec![
-           String::from("hi"),
-           String::from("from"),
-           String::from("the"),
-           String::from("thread"),
-       ];
-       for val in vals {
-           tx.send(val).unwrap();
-           thread::sleep(time::Duration::from_secs(1));
-
-       }
-   });
-   thread::spawn(move || {
-    let vals = vec![
-        String::from("more"),
-        String::from("messages"),
-        String::from("in"),
-        String::from("threadss"),
-    ];
-    for val in vals {
-        tx2.send(val).unwrap();
-        thread::sleep(time::Duration::from_secs(1));
-    }
-});
-   for received in rx {
-       println!("Got {}", received);
-   }
+    shared_states()
 }
